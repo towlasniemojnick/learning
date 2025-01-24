03cf993d5678421e319c813307e99a47ce194762
mateusz <m.dzieciatkowski@gmail.com>
2025-01-24 20:46:11 +0100

    webscrap and insert into dataframe

--- webscrapping/webscrap.py
+++ webscrapping/webscrap.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
 
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
@@ -12,11 +14,15 @@ results = soup.find(id="ResultsContainer")
 
 job_cards = results.find_all("div", class_="card-content")
 
+df = pd.DataFrame(columns=["Job", "Company", "Location"])
+
+print(df)
+
 for jc in job_cards:
     title_el = jc.find("h2", class_="title")
     company_el = jc.find("h3", class_="company")
     loc_el = jc.find("p", class_="location")
-    print(title_el)
-    print(company_el)
-    print(loc_el)
-    print()
+    df.loc[len(df)] = [title_el.text.strip(), company_el.text.strip(),
+                       loc_el.text.strip()]
+
+print(df.head())
